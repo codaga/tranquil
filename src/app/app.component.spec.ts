50c8c9c832c56ed2a0a35a2138718610f5d3299a
Davis O <davisokoth@gmail.com>
2019-08-20 16:15:17 +0300

    home: fix failing tests
    Add app-nav to the test-bed to fix failing tests
    Install ngMocks as a dev dep to use in mocking component dependencies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MockComponent, MockedComponent, MockRender } from 'ng-mocks';
+import { NavComponent } from './nav/nav.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MockComponent(NavComponent),
       ],
     }).compileComponents();
   }));
@@ -27,5 +30,12 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('tranquil');
+  });
+
+  it('should have a navbar in a nav tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-nav')).toBeDefined();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tranquil';
-  description = 'Better mental health'
+  description = 'Better mental health';
 }
