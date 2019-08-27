41d3e75e3195c01002f78e1c2a8ce6e6f5ed2f02
Davis O <davisokoth@gmail.com>
2019-08-27 22:11:02 +0300

    search: integrating search component
    Add `searchcomponent` to the navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { SpecialistDetailsComponent } from './specialist-details/specialist-details.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SpecialistsComponent } from './specialists/specialists.component';
+import { SearchComponent } from './search/search.component';
 
 
 const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo: 'home'},
     {path: 'home', component: HomeComponent},
+    {path: 'search', component: SearchComponent},
     {path: 'specialists', component: SpecialistsComponent},
     {path: 'specialists/:id', component: SpecialistDetailsComponent},
     {path: '**', component: NotFoundComponent}
